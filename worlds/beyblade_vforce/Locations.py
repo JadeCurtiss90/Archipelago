411c286493bc892414afffb87b2a168cdb1e7743
BootsinSoots <theron.swanson@gmail.com>
2026-08-18 23:20:55 -0400

    [Locations] Correct location data type hints

--- worlds/beyblade_vforce/Locations.py
+++ worlds/beyblade_vforce/Locations.py
@@ -18,9 +18,9 @@ class BVFUBJOptionData(NamedTuple):
 class  BVFUBJLocationData(NamedTuple):
     region: str
     location_groups: list[str]  # one or more groups that this location belongs to
-    access: Rule[Any] = None
-    req_options: BVFUBJOptionData = None
-    ram_data: Helpers_Function.RamData = None
+    access: Optional[Rule[Any]] = None
+    req_options: Optional[BVFUBJOptionData] = None
+    ram_data: Optional[Helpers_Function.RamData] = None
 
 
 class  BVFUBJLocation(Location):
